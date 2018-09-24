0956bf0f0a08a4fbd2f27c3378a2155d5f9205c1
Talha Balaj <talhabalaj@hotmail.com>
2018-09-24 20:22:01 +0500

    add 404

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const youtubeDl = require("ytdl-core");
+const youTubeDl = require("ytdl-core");
 const http = require("http");
 
 const server = http.createServer(handleServer);
 const PORT = process.env.PORT || 3000;
 
-const youtubeUrl = 'https://youtube.com/watch?v=';
+const youTubeUrl = 'https://youtube.com/watch?v=';
 
 const handleServer = (req, res) => {
     if (req.url.match(/\/[A-Z-a-z-0-9-_]{11}/gi)) {
-        const youtubeUrlOfVideo = youtubeUrl + req.url.split('/')[0];
-        youtubeDl.getInfo(youtubeUrlOfVideo, (err, info) => {
+        const youTubeUrlOfVideo = youTubeUrl + req.url.split('/')[0];
+        youTubeDl.getInfo(youTubeUrlOfVideo, (err, info) => {
             if (err) {
                 return res.end(JSON.stringify(err))
             }
             return res.end(JSON.stringify(info));
         })
     }
+    return res.end(JSON.stringify({
+        Error: "404 not found"
+    }))
 }
 
 server.listen(PORT)
